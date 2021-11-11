21a9b88fc9f8419eeae8af4f2ac1a15ca25119ae
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-11-11 17:41:10 -0500

    cleared code

--- Algo-leetcode/FizzBuzz.js
+++ Algo-leetcode/FizzBuzz.js
@@ -28,26 +28,3 @@ return result;
 
 fizzBuzz(n);
 fizzBuzz(6); //const n = 15
-var fizzBuzz = function(n){
-  //create a new array "result" to hold output
-  const result = [];
-
-  //create a loop for 1 to n
-  for(let i = 1; i <= n; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-      result.push("FizzBuzz");
-    }else if(i % 3 === 0){
-      result.push("Fizz");
-    }else if(i % 5 === 0){
-      result.push("Buzz");
-    }else{
-      result.push(i.toString())
-    }
-  }
-
-return result;
-}
-
-fizzBuzz(n);
-fizzBuzz(6); 
-
