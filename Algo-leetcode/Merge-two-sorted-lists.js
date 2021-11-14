3d4937157310d8352f16a5c295e28e4dc466fea4
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-11-13 20:43:06 -0500

    Remove duplicates from sorted array leetcode problem 26 solved

---
+++ Algo-leetcode/Merge-two-sorted-lists.js
@@ -0,0 +1,9 @@
+//Merge two sorted linked lists and return it as a sorted 
+// list should be made by splicing together the 
+// nodes of the first two lists.//
+//example1
+//Input: l1 = [1,2,4], l2 = [1,3,4]
+//Output: [1,1,2,3,4,4]
+//example2
+//Input: l1 = [], l2 = [0]
+// Output: [0]
